f5004808359c9db42c48336dd22c7c51fdc7652b
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2023-08-29 11:50:07 -0300

    Add files via upload

---
+++ Desafio-DiagramaUML/src/Equipamento/Musica/Ipod.java
@@ -0,0 +1,16 @@
+package src.Equipamento.Musica;
+
+public class Ipod implements ReproduzirMusica {
+    public void tocar() {
+        System.out.println("Tocar Musica");
+
+    }
+
+    public void pausar() {
+        System.out.println("Pause");
+    }
+
+    public void selecionarMusica() {
+        System.out.println("Selecionar musica");
+    }
+}
